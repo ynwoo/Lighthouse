bfaf1602f04e377d7550f265ac0aa46764edb716
신기정 <467shin@gmail.com>
2023-07-21 17:59:27 +0900

    fix: ESLint 세부 수정 완료

--- frontend/src/Routes/MainPage.js
+++ frontend/src/Routes/MainPage.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import SideComponent from '../components/Utils/SideComponent'
 import MainComponent from '../components/Utils/MainComponent'
 
 export default function MainPage() {
+  console.log(useContext)
   return (
     <div
       style={{
