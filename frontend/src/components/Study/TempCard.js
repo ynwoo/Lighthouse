542d18a944a1fd09f44270367c4861fde709687f
신기정 <467shin@gmail.com>
2023-07-24 17:35:09 +0900

    feat: add modules in store

--- frontend/src/components/Study/TempCard.js
+++ frontend/src/components/Study/TempCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 // 템플릿 카드
 
-export default function TempCard() {
+function TempCard() {
   return (
     // Temp Detail로 보내주는 링크
     // 그냥 컴포넌트 자체가 하나의 링크라고 보면 됨
@@ -20,3 +20,5 @@ export default function TempCard() {
     </Link>
   )
 }
+
+export default TempCard
