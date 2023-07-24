46ee09cd30291b81148a315af681cb5258aef815
imsongj <imsongj@gmail.com>
2023-07-24 10:53:03 +0900

    test: swagger 3.0 설정 추가 및 테스트 완료

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/LighthouseApplication.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/LighthouseApplication.java
@@ -2,7 +2,9 @@ package com.ssafy.lighthouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class LighthouseApplication {
 
