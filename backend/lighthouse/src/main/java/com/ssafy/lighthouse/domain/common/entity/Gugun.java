e0c43f9e1cdd7aee8e48c494cb5fb91f9423999d
ynwoo <ynwookim98@gmail.com>
2023-07-25 16:51:50 +0900

    fix: 사용되지 않는 import문 삭제

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/common/entity/Gugun.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/common/entity/Gugun.java
@@ -1,19 +1,16 @@
 package com.ssafy.lighthouse.domain.common.entity;
 
+import javax.persistence.Entity;
+
 import com.ssafy.lighthouse.domain.common.BaseEntity;
-import lombok.AccessLevel;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Gugun extends BaseEntity {
-    private Long sidoId;
-    private String name;
+	private Long sidoId;
+	private String name;
 }
