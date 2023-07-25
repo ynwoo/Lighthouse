930c6d0ae9597b4bfb98354aa3deabfea8eac43f
양승현 <haeundae96@gmail.com>
2023-07-25 11:15:50 +0900

    feat: chatting service improved with STOMP protocol

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/chat/controller/ChatController.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/chat/controller/ChatController.java
@@ -1,29 +1,110 @@
 package com.ssafy.lighthouse.domain.chat.controller;
 
-import com.ssafy.lighthouse.domain.chat.entity.ChatRoom;
-import com.ssafy.lighthouse.domain.chat.service.ChatService;
+import com.ssafy.lighthouse.domain.chat.dto.ChatDto;
+import com.ssafy.lighthouse.domain.chat.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.util.List;
 
-@RestController
+@Controller
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/chat")
+// 채팅 송신(Pub) 및 수신(Sub) 관리
 public class ChatController {
 
-    private final ChatService service;
+    // for use of convertAndSend -> automatically convert to Message object and send
+    private final SimpMessageSendingOperations template;
+    private final ChatRepository repository;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name, @RequestParam String id) {
-        log.debug("post name : {}, id : {}", name, id);
-        return service.createRoom(name, id);
+    // MessageMapping -> send msg via websocket
+    // client side: /pub/chat/message -> controller
+    // process ...
+    // server side: /sub/chat/room/{roomId} -> client
+    @MessageMapping("/chat/enterUser")
+    public void enterUser(@Payload ChatDto chat, SimpMessageHeaderAccessor headerAccessor) {
+
+        // user count += 1
+        repository.increaseUser(chat.getRoomId());
+
+        // add user to the room, return UUID
+        String userUUID = repository.addUser(chat.getRoomId(), chat.getSender());
+
+        // add userUUID to websocket session
+        headerAccessor.getSessionAttributes().put("userUUID", userUUID);
+        headerAccessor.getSessionAttributes().put("roomId", chat.getRoomId());
+
+        // send msg to all users in the room
+        chat.setMessage(chat.getSender() + "님이 입장하셨습니다.");
+        template.convertAndSend("/sub/chat/room/" + chat.getRoomId(), chat);
     }
 
-    @GetMapping
-    public List<ChatRoom> findAllRooms() {
-        return service.findAllRoom();
+    @MessageMapping("/chat/sendMessage")
+    public void sendMessage(@Payload ChatDto chat) {
+        log.debug("send message : {}", chat);
+        chat.setMessage(chat.getSender() + " : " + chat.getMessage());
+        template.convertAndSend("/sub/chat/room/" + chat.getRoomId(), chat);
     }
+
+    @EventListener
+    public void webSocketDisconnectListener(SessionDisconnectEvent event) {
+        log.debug("web socket disconnect listener : {}", event);
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        // uuid, roomid from stomp session -> temove user from the room
+        String userUUID = (String) headerAccessor.getSessionAttributes().get("userUUID");
+        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
+
+        log.debug("headerAccessor : {}", headerAccessor);
+
+        // decrease user count
+        repository.decreaseUser(roomId);
+
+        // remove user from the chatting room
+        String userName = repository.getUserName(roomId, userUUID);
+        repository.deleteUser(roomId, userUUID);
+
+        if (userName != null) {
+            log.debug("user {} is disconnected from room {}", userName, roomId);
+
+            ChatDto chat = ChatDto.builder()
+                    .type(ChatDto.MessageType.LEAVE)
+                    .sender(userName)
+                    .message(userName + "님이 퇴장하셨습니다.")
+                    .build();
+
+            template.convertAndSend("/sub/chat/room/" + roomId, chat);
+        }
+
+    }
+
+    // return users in the room
+    @GetMapping("/chat/userlist")
+    @ResponseBody
+    public List<String> userList(String roomId) {
+        return repository.getUserList(roomId);
+    }
+
+    // check user name duplicate
+    @GetMapping("/chat/duplicateName")
+    @ResponseBody
+    public String isDuplicateName(@RequestParam("roomId") String roomId, @RequestParam("username") String username) {
+        String userName = repository.isDuplicateName(roomId, username);
+        log.debug("isDuplicateName : {}", userName);
+
+        return userName;
+    }
+
 }
--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/chat/entity/ChatRoom.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/chat/entity/ChatRoom.java
@@ -1,44 +1,24 @@
 package com.ssafy.lighthouse.domain.chat.entity;
 
-import com.ssafy.lighthouse.domain.chat.dto.ChatDto;
-import com.ssafy.lighthouse.domain.chat.service.ChatService;
-import lombok.Builder;
 import lombok.Data;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.UUID;
 
 @Data
 public class ChatRoom {
+
     private String roomId;
-    private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
+    private String roomName;
+    private long userCount;
 
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
-    }
+    private HashMap<String, String> userList = new HashMap<>();
 
-    public void handleAction(WebSocketSession session, ChatDto message, ChatService service) {
-        if (message.getType().equals(ChatDto.MessageType.ENTER)) {
-            sessions.add(session);
-            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-            sendMessage(message, service);
+    public ChatRoom create(String roomName) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.roomName = roomName;
 
-        } else if (message.getType().equals(ChatDto.MessageType.LEAVE)) {
-            sessions.remove(session);
-            message.setMessage(message.getSender() + "님이 퇴장하셨습니다.");
-            sendMessage(message, service);
-
-        } else {
-            message.setMessage(message.getSender() + " : " + message.getMessage());
-            sendMessage(message, service);
-        }
-    }
-
-    public <T> void sendMessage(T message, ChatService service) {
-        sessions.parallelStream().forEach(sessions -> service.sendMessage(sessions, message));
+        return chatRoom;
     }
 }
