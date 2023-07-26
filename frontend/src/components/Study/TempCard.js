2876f3d27e8984c51b81df276a704a2569391eb5
신기정 <467shin@gmail.com>
2023-07-26 09:22:27 +0900

    config: frontend branch 생성 및 테스트

--- frontend/src/components/Study/TempCard.js
+++ frontend/src/components/Study/TempCard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 // 템플릿 카드
 
 function TempCard({ study }) {
+  console.log(study)
   return (
     // Temp Detail로 보내주는 링크
     // 그냥 컴포넌트 자체가 하나의 링크라고 보면 됨
@@ -21,6 +22,7 @@ function TempCard({ study }) {
         <p>
           {study.current_member}/{study.max_member}
         </p>
+        <p>{study.is_online ? '온라인' : '오프라인'}</p>
         <p>{study.like_cnt}개의 따봉</p>
       </div>
     </Link>
