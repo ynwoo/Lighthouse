da9f88107c441a1904ab96b2c3422d83cd4bfb91
양승현 <haeundae96@gmail.com>
2023-07-31 12:26:05 +0900

    feat: heartbeat optimization complete by using double queue swapping. working well

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/heartbeat/controller/HeartbeatController.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/heartbeat/controller/HeartbeatController.java
@@ -2,6 +2,7 @@ package com.ssafy.lighthouse.domain.heartbeat.controller;
 
 import com.ssafy.lighthouse.domain.heartbeat.service.HeartbeatService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class HeartbeatController {
 
     private final HeartbeatService heartbeatService;
 
     @GetMapping("/heartbeat/{id}")
     public ResponseEntity<Object> heartbeat(@PathVariable("id") Long id) {
+        log.debug("heartbeat: " + id + " " + System.currentTimeMillis());
         heartbeatService.updateHeartbeat(id);
         return ResponseEntity.ok().build();
     }
--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/heartbeat/service/HeartbeatService.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/heartbeat/service/HeartbeatService.java
@@ -1,6 +1,10 @@
 package com.ssafy.lighthouse.domain.heartbeat.service;
 
+import com.ssafy.lighthouse.domain.heartbeat.entity.Heartbeat;
+import com.ssafy.lighthouse.domain.heartbeat.repository.HeartbeatRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -9,28 +13,52 @@ import java.util.HashMap;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class HeartbeatService {
 
-    private final int repeatMilisecond = 1000 * 5;
-    private final int offlineThreshMilisecond = 1000 * 10;
-    private final HashMap<Long, Timestamp> userHeartbeat = new HashMap<Long, Timestamp>();
+    private final HeartbeatRepository heartbeatRepository;
+
+    private final int repeatMilisecond = 1000 * 20;
+    private HashMap<Long, Heartbeat> checkPending = new HashMap<Long, Heartbeat>();
+    private HashMap<Long, Heartbeat> aliveChecked = new HashMap<Long, Heartbeat>();
+
 
     public void updateHeartbeat(Long id) {
-        userHeartbeat.put(id, new Timestamp(System.currentTimeMillis()));
+        Heartbeat beat;
+
+        // case 1: user alive: pending -> active
+        if (checkPending.containsKey(id)) {
+            beat = checkPending.get(id);
+            checkPending.remove(id);
+
+            beat.setActiveAt(System.currentTimeMillis());
+            aliveChecked.put(id, beat);
+        // case 2: user already alive: active -> active
+        } else if (aliveChecked.containsKey(id)) {
+            beat = aliveChecked.get(id);
+            beat.setActiveAt(System.currentTimeMillis());
+        // case 3: new user: new -> active
+        } else {
+            beat = new Heartbeat(id);
+            aliveChecked.put(id, beat);
+            heartbeatRepository.save(beat);
+        }
+
     }
 
     @Scheduled(fixedRate = repeatMilisecond)
     public void checkOnlineTimer() {
-        Timestamp now = new Timestamp(System.currentTimeMillis());
-
-        // 현재 시간을 기준으로 repeatMilisecond 이상 heartbeat이 없는 유저를 오프라인으로 처리
-        for (Long id : userHeartbeat.keySet()) {
-            Timestamp lastHeartbeat = userHeartbeat.get(id);
-            if (now.getTime() - lastHeartbeat.getTime() > offlineThreshMilisecond) {
-                // 오프라인 처리됨
-                System.out.println("오프라인 처리됨: " + id);
-                userHeartbeat.remove(id);
-            }
+        // to be offline: pending timeout
+        for (Long id: checkPending.keySet()) {
+            // send offline signal to client via web socket
+            /*
+            CODE HERE
+             */
+            log.debug("오프라인 처리됨: " + id);
         }
+
+        // swap pending & checked queue
+        checkPending = aliveChecked;
+        aliveChecked = new HashMap<Long, Heartbeat>();
     }
 }
