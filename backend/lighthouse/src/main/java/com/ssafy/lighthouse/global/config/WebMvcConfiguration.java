1c285ae958ef82731fcd59c09eca949bf4160114
taegn <taegyunn123@gmail.com>
2023-08-01 14:28:30 +0900

    cors 수정

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/global/config/WebMvcConfiguration.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/global/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.ssafy.lighthouse.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedOrigins("http://localhost:8080", "i9a409.p.ssafy.io:8081")
+                .allowedMethods(HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());
     }
 }
