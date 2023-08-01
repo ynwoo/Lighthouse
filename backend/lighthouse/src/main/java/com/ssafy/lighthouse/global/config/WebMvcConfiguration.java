a26830795e1ed9823b603285486862a2df43c0e2
ynwoo <ynwookim98@gmail.com>
2023-08-01 16:25:14 +0900

    fix: addCorsMappings 관련 에러 해결

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/global/config/WebMvcConfiguration.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/global/config/WebMvcConfiguration.java
@@ -1,18 +1,17 @@
 package com.ssafy.lighthouse.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowCredentials(true);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*")
+			.allowedMethods("*")
+			.allowCredentials(true);
+	}
 }
