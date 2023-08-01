27e95e71825f8957cba4eb9c9a2b06f5f29803d5
노태균 <taegyunn123@gmail.com>
2023-08-02 08:51:19 +0900

    revert: user -> sido, gugun, tag rollback

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/user/entity/UserTag.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/user/entity/UserTag.java
@@ -1,43 +1,36 @@
 package com.ssafy.lighthouse.domain.user.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 
 import com.ssafy.lighthouse.domain.common.BaseEntity;
 
-import com.ssafy.lighthouse.domain.common.entity.Tag;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
-@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserTag extends BaseEntity {
 	private Long userId;
+	private Long tagId;
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "tagId")
-	private Tag tag;
-
-	public void setUserId(Long userId) {
+	@Builder
+	public UserTag(Long userId, Long tagId) {
 		this.userId = userId;
+		this.tagId = tagId;
 	}
 
-//	public void updateUserTagInfo(Long userId, Long tagId) {
-//		this.userId = userId;
-//		this.tag = tagId;
-//	}
+	public void updateUserTagInfo(Long userId, Long tagId) {
+		this.userId = userId;
+		this.tagId = tagId;
+	}
 
-//	public static UserTag from(Long id, Long tagId) {
-//		return UserTag.builder()
-//			.userId(id)
-//			.tagId(tagId)
-//			.build();
-//	}
+	public static UserTag from(Long id, Long tagId) {
+		return UserTag.builder()
+			.userId(id)
+			.tagId(tagId)
+			.build();
+	}
 }
