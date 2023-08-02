7e2cfe5b696dacff4c32f90a02f6d96d5e71bf22
노태균 <taegyunn123@gmail.com>
2023-08-02 16:38:55 +0900

    feat: 템플릿 사용 스터디 리스트 구현

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/study/repository/StudyRepository.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/study/repository/StudyRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.lighthouse.domain.study.repository;
 
+import com.ssafy.lighthouse.domain.study.dto.SimpleStudyDto;
 import com.ssafy.lighthouse.domain.study.entity.Study;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryCustom {
@@ -21,4 +25,8 @@ public interface StudyRepository extends JpaRepository<Study, Long>, StudyReposi
     // 단순 조회 (공유, 삭제용)
     @Query("select s from Study s where s.id = :studyId and s.isValid = 1")
     Optional<Study> findById(@Param("studyId") Long studyId);
+
+    // 이전 스터디 목록 (좋아요 순)
+    @Query("select new com.ssafy.lighthouse.domain.study.dto.SimpleStudyDto(s) from Study s where s.original.id = :originalId and s.isValid = 1 and s.status = 3 order by s.likeCnt desc ")
+    Page<SimpleStudyDto> findAllByOriginalId(@Param("originalId") Long originalId, Pageable pageable);
 }
