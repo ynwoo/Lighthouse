ff5cf6939e3630121afa123472d40c2dfd262fcf
peachez-z <rkfld0911@gmail.com>
2023-08-03 14:03:50 +0900

    feat : study info 갈아엎기

--- frontend/src/components/Study/memo/MemoList.jsx
+++ frontend/src/components/Study/memo/MemoList.jsx
@@ -1,12 +1,17 @@
 // MemoList.jsx
 import React from 'react'
 
-function MemoList({ memos }) {
+function MemoList({ memos, onDeleteMemo }) {
   return (
     <div style={{ width: '50%' }}>
       <ul>
         {memos.map(memo => (
-          <li key={memo.id}>{memo.text}</li>
+          <li key={memo.id}>
+            {memo.text}
+            <button type="submit" onClick={() => onDeleteMemo(memo.id)}>
+              삭제
+            </button>
+          </li>
         ))}
       </ul>
     </div>
