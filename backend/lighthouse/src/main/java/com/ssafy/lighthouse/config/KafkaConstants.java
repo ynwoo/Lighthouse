d556b431152affd930f2490fcc3898ff7df9d888
양승현 <haeundae96@gmail.com>
2023-08-03 14:44:07 +0900

    test: test kafka connection via different port: 29092

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/config/KafkaConstants.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/config/KafkaConstants.java
@@ -4,5 +4,5 @@ public class KafkaConstants {
     public static final String KAFKA_TOPIC = "kafka-chat";
     public static final String GROUP_PROPAGATE = "propagate";
     public static final String GROUP_STORE = "store";
-    public static final String KAFKA_BROKER = "i9a409.p.ssafy.io:9092";
+    public static final String KAFKA_BROKER = "i9a409.p.ssafy.io:29092";
 }
