b933078743f1dddc6e01758aa846d258e6e979e1
노태균 <taegyunn123@gmail.com>
2023-08-04 16:01:32 +0900

    feat: userBadge 구현 & study 종료 시 참여자에게 badge 전달 구현

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/common/repository/BadgeRepository.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/common/repository/BadgeRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
     @Query("select b from Badge b where b.id = :badgeId and b.isValid = 1")
     Optional<Badge> findByBadgeId(@Param("badgeId") Long badgeId);
+    
+    // badgeId list에 해당하는 badge List 리턴
+    @Query("select b from Badge b where b.id in (:badgeIds) and b.isValid = 1 order by b.createdAt desc ")
+    List<Badge> findAllByBadgeIds(@Param("badgeIds") List<Long> badgeIds);
 }
