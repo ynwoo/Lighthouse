b9c21eac7144d9a8d393a7541c0c59d6b2dcc114
taegn <taegyunn123@gmail.com>
2023-08-04 09:54:02 +0900

    studyBadge 구현

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/study/repository/StudyRepository.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/study/repository/StudyRepository.java
@@ -14,11 +14,11 @@ import java.util.Optional;
 
 public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryCustom {
     // 상세 정보 조회 (상세정보)
-    @EntityGraph(attributePaths = {"original", "sido", "gugun", "studyTags", "studyEvals", "studyNotices", "sessions", "qnas", "participations"})
+    @EntityGraph(attributePaths = {"original", "sido", "gugun", "badge", "studyTags", "studyEvals", "studyNotices", "sessions", "qnas", "participations"})
     Optional<Study> findDetailById(@Param("studyId") Long studyId);
 
     // 상세 정보 조회 (복제)
-    @EntityGraph(attributePaths = {"sido", "gugun", "studyTags", "studyNotices", "sessions"})
+    @EntityGraph(attributePaths = {"sido", "gugun", "badge", "studyTags", "studyNotices", "sessions"})
     @Query("select s from Study s where s.id = :studyId and s.isValid = 1")
     Optional<Study> findSimpleDetailById(@Param("studyId") Long studyId);
 
