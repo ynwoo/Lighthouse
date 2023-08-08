a9c9ca894e58e2b79844612e3cd8d5f378014ec9
yoonwoo-kim <ynwookim98@gmail.com>
2023-08-08 23:48:56 +0900

    refactor: 구글 로그인 시 session으로 값 전달

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/auth/controller/AuthController.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/auth/controller/AuthController.java
@@ -1,10 +1,17 @@
 package com.ssafy.lighthouse.domain.auth.controller;
 
+import com.ssafy.lighthouse.domain.auth.dto.OAuthTokenDto;
+import com.ssafy.lighthouse.domain.auth.dto.OAuthUserInfoDto;
+import com.ssafy.lighthouse.domain.auth.service.OAuthService;
+import com.ssafy.lighthouse.domain.user.dto.AlertDto;
+import com.ssafy.lighthouse.domain.user.entity.User;
+import com.ssafy.lighthouse.domain.user.service.JwtService;
+import com.ssafy.lighthouse.domain.user.service.UserService;
 import java.net.URI;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,105 +21,92 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.lighthouse.domain.auth.dto.OAuthTokenDto;
-import com.ssafy.lighthouse.domain.auth.dto.OAuthUserInfoDto;
-import com.ssafy.lighthouse.domain.auth.service.OAuthService;
-import com.ssafy.lighthouse.domain.user.entity.User;
-import com.ssafy.lighthouse.domain.user.service.JwtService;
-import com.ssafy.lighthouse.domain.user.service.UserService;
-
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @Slf4j
 @RequestMapping("/auth")
 public class AuthController {
-	private final UserService userService;
-	private final OAuthService oAuthService;
-	private final JwtService jwtService;
 
-	@Autowired
-	public AuthController(OAuthService oAuthService, UserService userService, JwtService jwtService) {
-		this.oAuthService = oAuthService;
-		this.userService = userService;
-		this.jwtService = jwtService;
-	}
+    private final UserService userService;
+    private final OAuthService oAuthService;
+    private final JwtService jwtService;
 
-	@GetMapping("/callback/google")
-	public ResponseEntity<?> successGoogleLogin(
-		@RequestParam("code") String accessCode,
-		HttpServletResponse response) {
-		// Map<String, Object> resultMap = new HashMap<>();
-		// HttpStatus status;
+    @Autowired
+    public AuthController(OAuthService oAuthService, UserService userService,
+            JwtService jwtService) {
+        this.oAuthService = oAuthService;
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
 
-		OAuthTokenDto oAuthTokenDto = oAuthService.getGoogleAccessToken(accessCode);
-		OAuthUserInfoDto userInitialInfo = oAuthService.getUserInfo(oAuthTokenDto);
-		System.out.println(userInitialInfo);
+    @GetMapping("/callback/google")
+    public ResponseEntity<?> successGoogleLogin(
+            @RequestParam("code") String accessCode,
+            HttpServletRequest request) {
 
-		// providerID로 User 존재하는지 확인
-		User userEntity = userService.getUserByProviderId(userInitialInfo.getProviderId());
-		if (userEntity == null) { // 새로운 유저 -> User 테이블에 저장
-			userEntity = userService.addOAuthUser(userInitialInfo);
-		} else if (userEntity.getIsValid() == 0) {
-			userEntity.updateIsvalid();
-		}
+        OAuthTokenDto oAuthTokenDto = oAuthService.getGoogleAccessToken(accessCode);
+        OAuthUserInfoDto userInitialInfo = oAuthService.getUserInfo(oAuthTokenDto);
 
-		// jwt 토큰 발급
-		String accessToken = jwtService.createAccessToken("userId", userEntity.getId());
-		String refreshToken = jwtService.createRefreshToken("userId", userEntity.getId());
+        // providerID로 User 존재하는지 확인
+        User userEntity = userService.getUserByProviderId(userInitialInfo.getProviderId());
+        if (userEntity == null) { // 새로운 유저 -> User 테이블에 저장
+            userEntity = userService.addOAuthUser(userInitialInfo);
+        } else if (userEntity.getIsValid() == 0) { // 회원탈퇴했던 유저라면 복귀
+            userEntity.updateIsvalid();
+        }
 
-		try {
-			userService.saveRefreshToken(userEntity.getId(), refreshToken);
-			log.debug("소셜 로그인 accessToken 정보 : {}", accessToken);
-			log.debug("소셜 로그인 refreshToken 정보 : {}", refreshToken);
+        // jwt 토큰 발급
+        String accessToken = jwtService.createAccessToken("userId", userEntity.getId());
+        String refreshToken = jwtService.createRefreshToken("userId", userEntity.getId());
 
-			// 알림 목록 불러오기
-			// List<AlertDto> alertDtoList = userService.getAlertDtoList(userEntity.getId());
-			// resultMap.put("alerts", alertDtoList);
+        try {
+            userService.saveRefreshToken(userEntity.getId(), refreshToken);
+            log.debug("소셜 로그인 accessToken 정보 : {}", accessToken);
+            log.debug("소셜 로그인 refreshToken 정보 : {}", refreshToken);
 
-			Cookie c1 = makeCookie("access_token", accessToken);
-			response.addCookie(c1);
+            // 알림 목록 불러오기
+            List<AlertDto> alertDtoList = userService.getAlertDtoList(userEntity.getId());
+            HttpSession session = request.getSession();
+            // 세션에 로그인 회원정보 보관
+            session.setAttribute("access_token", accessToken);
+            session.setAttribute("refresh_token", refreshToken);
+            session.setAttribute("userId", userEntity.getId().toString());
+            session.setAttribute("alerts", alertDtoList);
+//            Cookie c1 = makeCookie("access_token", accessToken);
+//            response.addCookie(c1);
+//
+//            Cookie c2 = makeCookie("refresh_token", refreshToken);
+//            response.addCookie(c2);
+//
+//            Cookie c3 = makeCookie("user_id", userEntity.getId().toString());
+//            response.addCookie(c3);
 
-			Cookie c2 = makeCookie("refresh_token", refreshToken);
-			response.addCookie(c2);
+            // StringBuilder sb = new StringBuilder();
+            // for (AlertDto alertDto : alertDtoList) {
+            // 	System.out.println(alertDto);
+            // 	sb.append(alertDto);
+            // }
+            // System.out.println(sb);
+            // log.debug(sb.toString());
+            // Cookie c4 = makeCookie("alerts", sb.toString());
+            // response.addCookie(c4);
 
-			Cookie c3 = makeCookie("user_id", userEntity.getId().toString());
-			response.addCookie(c3);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        HttpHeaders headers = new HttpHeaders();
 
-			// StringBuilder sb = new StringBuilder();
-			// for (AlertDto alertDto : alertDtoList) {
-			// 	System.out.println(alertDto);
-			// 	sb.append(alertDto);
-			// }
-			// System.out.println(sb);
-			// log.debug(sb.toString());
-			// Cookie c4 = makeCookie("alerts", sb.toString());
-			// response.addCookie(c4);
+        // 프론트 서버로 리다이렉트
+        headers.setLocation(URI.create("http://i9a409.p.ssafy.io:3000/"));
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    }
 
-			// resultMap.put("user-id", userEntity.getId());
-			// resultMap.put("access-token", accessToken);
-			// resultMap.put("refresh-token", refreshToken);
-			// resultMap.put("message", SUCCESS);
-
-			// status = HttpStatus.OK;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		HttpHeaders headers = new HttpHeaders();
-
-		// 프론트 서버로 리다이렉트
-		headers.setLocation(URI.create("http://i9a409.p.ssafy.io:3000/"));
-		return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-		// return new ResponseEntity<>(resultMap, status);
-	}
-
-	private Cookie makeCookie(String name, String value) {
-		Cookie cookie = new Cookie(name, value);
-		cookie.setMaxAge(60 * 60 * 24 * 7);
-		cookie.setHttpOnly(true);
-		cookie.setPath("/");
-
-		return cookie;
-	}
+//    private Cookie makeCookie(String name, String value) {
+//        Cookie cookie = new Cookie(name, value);
+//        cookie.setMaxAge(60 * 60 * 24 * 7);
+//        cookie.setHttpOnly(true);
+//        cookie.setPath("/");
+//
+//        return cookie;
+//    }
 
 }
