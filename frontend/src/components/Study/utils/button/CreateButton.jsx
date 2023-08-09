85dc1dbe71ce9a1b9a388a9e842d9d4718b5146a
taegn <taegyunn123@gmail.com>
2023-08-09 17:25:12 +0900

    button

--- frontend/src/components/Study/utils/button/CreateButton.jsx
+++ frontend/src/components/Study/utils/button/CreateButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Button.css'
+import styles from './Button.module.css'
 
 export default function CreateButton({ onClick, children }) {
   return (
-    <button className="button-create" type="button" onClick={onClick}>
+    <button className={styles.button} type="button" onClick={onClick}>
       {children ?? '등록'}
     </button>
   )
--- frontend/src/components/Study/utils/button/UpdateButton.jsx
+++ frontend/src/components/Study/utils/button/UpdateButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Button.css'
+import styles from './Button.module.css'
 
 export default function UpdateButton({ onClick, children }) {
   return (
-    <button className="button-create" type="button" onClick={onClick}>
+    <button className={styles.button} type="button" onClick={onClick}>
       {children ?? '수정'}
     </button>
   )
