784c5ae99c764d24d209a58847e8f795f414d978
taegn <taegyunn123@gmail.com>
2023-08-10 12:57:43 +0900

    login시 데이터 수정

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/user/repository/UserRepositoryCustom.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/user/repository/UserRepositoryCustom.java
@@ -2,12 +2,14 @@ package com.ssafy.lighthouse.domain.user.repository;
 
 import com.ssafy.lighthouse.domain.user.dto.ProfileResponse;
 import com.ssafy.lighthouse.domain.user.dto.SimpleProfileResponse;
+import com.ssafy.lighthouse.domain.user.dto.SimpleUserResponse;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Set;
 
 public interface UserRepositoryCustom {
     ProfileResponse findProfileByUserId(Long userId, Long loginId);
     SimpleProfileResponse findSimpleProfileByUserId(Long userId);
     List<SimpleProfileResponse> findSimpleProfileByUserIds(List<Long> userIds);
+    SimpleUserResponse findUserInfo(@Param("userId") Long userId);
 }
