01f6238ee70a53ace0bfe733c165205fa5921038
양승현 <haeundae96@gmail.com>
2023-08-10 10:36:23 +0900

    Feature/chat

--- frontend/src/store/chat.js
+++ frontend/src/store/chat.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
+import { Client } from '@stomp/stompjs'
 
 const initialState = {
   messages: [],
@@ -9,17 +10,46 @@ export const chatAction = {
     try {
       const response = await axios
         .post(`http://i9a409.p.ssafy.io:8081/kafka/publish`, {
-          type: 'TALK',
-          roomId: '1',
-          senderId: '777',
-          senderName: 'shin',
-          message: payload,
+          type: payload.type,
+          roomId: payload.roomId,
+          senderId: payload.senderId,
+          senderName: payload.userName,
+          message: payload.text,
         })
-        .then(console.log('발송 성공'))
+        .then(console.log('payload 발송 성공 +=> \n', payload))
       return thunkAPI.fulfillWithValue(response.data)
     } catch (err) {
       return thunkAPI.rejectWithValue(err)
     }
+  }),
+  connectSocket: createAsyncThunk('chat/connect', async (payload, thunkAPI) => {
+    try {
+      const client = new Client({
+        brokerURL: `ws://i9a409.p.ssafy.io:8082/ws/chat`,
+        connectHeaders: {
+          login: 'user',
+          passcode: 'password',
+        },
+        debug(callbackLog) {
+          console.log(`connection: ${payload}  ${callbackLog}`)
+        },
+        reconnectDelay: 5000, // 자동 재 연결
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+      })
+
+      return client
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err)
+    }
+
+    // client.onConnect = function (frame) {
+    //   console.log('Connected: ' + frame)
+    //   client.subscribe('/topic/public', function (message) {
+    //     console.log(message.body)
+    //     thunkAPI.dispatch(receiveMessage(message.body))
+    //   })
+    // }
   }),
 }
 export const chatSlice = createSlice({
