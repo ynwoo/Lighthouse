57189e07dd659513f672a76c27717f61e64ee88b
peachez-z <rkfld0911@gmail.com>
2023-08-10 15:12:29 +0900

    머지를 위한 커밋

--- frontend/src/components/Study/StudyQnA.jsx
+++ frontend/src/components/Study/StudyQnA.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Collapse } from 'antd'
 
 function QnA({ qnas }) {
   return (
@@ -6,14 +7,41 @@ function QnA({ qnas }) {
     <>
       <div className="big_box">
         {qnas?.map(qna => (
-          <p>
-            <p>
-              <h3>Q : {qna.question}</h3>
-              {qna.userId} : {qna.createdAt}
-            </p>
-            <p>A : {qna.answer}</p>
-            <hr />
-          </p>
+          <Collapse
+            size="large"
+            items={[
+              {
+                key: '1',
+                label: (
+                  <p>
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      {' '}
+                      <h3 style={{ margin: '0px' }}>Q : {qna.question}</h3>
+                      <p>작성 시간 : {qna.createdAt}</p>
+                    </div>
+
+                    <p style={{ textAlign: 'right' }}> 작성자 : {qna.userId}</p>
+                  </p>
+                ),
+                children: (
+                  <h3 style={{ textAlign: 'left' }}>A : {qna.answer}</h3>
+                ),
+              },
+            ]}
+          />
+          // <p>
+          //   <p>
+          //     <h3>Q : {qna.question}</h3>
+          //     {qna.userId} : {qna.createdAt}
+          //   </p>
+          //   <p>A : {qna.answer}</p>
+          //   <hr />
+          // </p>
         ))}
       </div>
     </>
