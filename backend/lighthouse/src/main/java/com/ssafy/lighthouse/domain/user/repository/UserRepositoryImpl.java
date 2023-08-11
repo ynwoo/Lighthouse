598f8bea029a2adf25f12b98700c3a0fbb0ce7a7
노태균 <taegyunn123@gmail.com>
2023-08-11 13:29:57 +0900

    refactor: userProfile -> study status에 따른 분리 부분 수정

--- backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/user/repository/UserRepositoryImpl.java
+++ backend/lighthouse/src/main/java/com/ssafy/lighthouse/domain/user/repository/UserRepositoryImpl.java
@@ -46,20 +46,11 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
 
     @Override
     public ProfileResponse findProfileByUserId(Long userId, Long loginId) {
-        Set<Long> participatedSet = userId.equals(loginId) ? participationHistoryRepository.findStudyIdAllByUserId(userId, STATUS.PREPARING) : new HashSet<>();
-        Set<Long> recruitingSet = participationHistoryRepository.findStudyIdAllByUserId(userId, STATUS.RECRUITING);
-        Set<Long> progressSet = participationHistoryRepository.findStudyIdAllByUserId(userId, STATUS.PROGRESS);
-        Set<Long> terminatedSet = participationHistoryRepository.findStudyIdAllByUserId(userId, STATUS.TERMINATED);
-        Set<Long> bookmarkSet = bookmarkRepository.findAllByUserId(userId);
-
         // all
-        Set<Long> allStudyIdSet = new HashSet<>();
-        allStudyIdSet.addAll(participatedSet);
-        allStudyIdSet.addAll(recruitingSet);
-        allStudyIdSet.addAll(progressSet);
-        allStudyIdSet.addAll(terminatedSet);
-        allStudyIdSet.addAll(bookmarkSet);
-
+        Set<Long> allStudyIdSet = participationHistoryRepository.findStudyIdAllByUserId(userId);
+        // bookmark studyIdSet
+        Set<Long> bookmarkSet = bookmarkRepository.findAllByUserId(userId);
+        // tag
         Set<Long> tagSet = userTagRepository.findTagIdAllByUserId(userId);
         QFollow followee = new QFollow("followee");
 
@@ -77,25 +68,30 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
                     .id(study.getLeaderId())
                     .build());
 
-            // 신청한 스터디
-            if (participatedSet.contains(study.getId())) {
-                participatedStudies.add(simpleStudyDto);
-            }
+            // status에 따른 스터디 분류
+            switch(study.getStatus()) {
+                // 신청한 스터디
+                case STATUS.PREPARING:
+                    if(userId.equals(loginId)) {
+                        participatedStudies.add(simpleStudyDto);
+                    }
+                    break;
 
-            // 모집중 스터디
-            else if(recruitingSet.contains(study.getId())) {
-                recruitingStudies.add(simpleStudyDto);
-            }
+                // 모집중 스터디
+                case STATUS.RECRUITING:
+                    recruitingStudies.add(simpleStudyDto);
+                    break;
 
-            // 진행중 스터디
-            else if(progressSet.contains(study.getId())) {
-                progressStudies.add(simpleStudyDto);
-            }
+                // 진행중 스터디
+                case STATUS.PROGRESS:
+                    progressStudies.add(simpleStudyDto);
+                    break;
 
-            // 끝난 스터디
-            else if(terminatedSet.contains(study.getId())) {
-                terminatedStudies.add(simpleStudyDto);
-            } 
+                // 끝난 스터디
+                case STATUS.TERMINATED: case STATUS.SHARE:
+                    terminatedStudies.add(simpleStudyDto);
+                    break;
+            }
 
             // 북마크 한 스터디
             if(bookmarkSet.contains(study.getId())) {
