d5ef1e701268951242d9487a37b8203cc3db7ad5
노태균 <taegyunn123@gmail.com>
2023-08-11 10:43:19 +0900

    feat: studyDetail 검색 구현

--- frontend/src/components/Study/utils/button/NextButton.jsx
+++ frontend/src/components/Study/utils/button/NextButton.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import styles from './Button.module.css'
 
-export default function Button({ onClick, children }) {
+export default function NextButton({ onClick, children }) {
   return (
     <button className={styles.button} type="button" onClick={onClick}>
       {children}
