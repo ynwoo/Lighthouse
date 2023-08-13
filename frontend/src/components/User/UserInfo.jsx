7c5cccc428ab3830de43c8b0cd454cceda2cb78e
노태균 <taegyunn123@gmail.com>
2023-08-13 21:52:02 +0900

    fix: userInfo reduce관련 에러 해결

--- frontend/src/components/User/UserInfo.jsx
+++ frontend/src/components/User/UserInfo.jsx
@@ -12,16 +12,25 @@ export default function UserInfo({ profile }) {
         <p>{profile.description}</p>
       </Card>
       <Card title="통계" bordered={false}>
+<<<<<<< HEAD
+        <p>현재 진행 예정인 스터디: {profile.recruitingStudies.length}</p>
+        <p>현재 진행 중인 스터디: {profile.progressStudies.length}</p>
+=======
         <p>현재 진행 예정인 스터디: {profile.progressStudies.length}</p>
         <p>현재 진행 중인 스터디: {profile.recruitingStudies.length}</p>
+>>>>>>> b4c12ee9939556ab8e9abfac09951bbb0b603599
         <p>완료한 스터디: {profile.terminatedStudies.length}</p>
         <p>북마크한 스터디: {profile.bookmarkStudies.length}</p>
         <p>
           함께한 스터디원:{' '}
           {profile.recruitingStudies
             .concat(profile.terminatedStudies)
+<<<<<<< HEAD
+            .reduce((a, b) => a + b.currentMember, 0)}
+=======
             .map(study => study.currentMember)
             .reduce((a, b) => a + b)}
+>>>>>>> b4c12ee9939556ab8e9abfac09951bbb0b603599
         </p>
         <p>
           스터디장 맡은 횟수:{' '}
