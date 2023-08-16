e34fe25bb6ab05ac96a4d1c3790c201b529a20e0
노태균 <taegyunn123@gmail.com>
2023-08-16 15:02:40 +0900

    refactor: study status를 가독성을 높이기 위해 숫자 ->  변수로 변경

--- frontend/src/components/Study/utils/button/CreateButton.jsx
+++ frontend/src/components/Study/utils/button/CreateButton.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 
 export default function CreateButton({ onClick, children }) {
   return (
-    <button className={styles.button} type="button" onClick={onClick}>
+    <button className={styles['button-create']} type="button" onClick={onClick}>
       {children ?? '등록'}
     </button>
   )
