e60d1d21ebb382f83147c5caa2469b53c4778c44
노태균 <taegyunn123@gmail.com>
2023-08-17 14:21:13 +0900

    fix: studyList default 정렬 순서 -> createdAt asc 변경

--- frontend/src/components/Study/utils/button/CreateButton.jsx
+++ frontend/src/components/Study/utils/button/CreateButton.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Button.module.css'
 
-export default function CreateButton({ onClick, children }) {
+export default function CreateButton({ onClick, color, children }) {
   return (
-    <button className={styles['button-create']} type="button" onClick={onClick}>
+    <button
+      className={[styles['button-create'], styles[`${color}`]].join(' ')}
+      type="button"
+      onClick={onClick}
+    >
       {children ?? '등록'}
     </button>
   )
